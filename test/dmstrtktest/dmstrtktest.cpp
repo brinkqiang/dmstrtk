5218e4612ac7cdede5b58e9520e59b9192f1da1b
brinkqiang <brink.qiang@gmail.com>
2025-02-13 23:21:25 +0800

    add

--- test/dmstrtktest/dmstrtktest.cpp
+++ test/dmstrtktest/dmstrtktest.cpp
@@ -3,5 +3,19 @@
 
 int main( int argc, char* argv[] )
 {
+    std::deque<std::string> str_lst;
+
+    str_lst.push_back("12,32,12");
+    str_lst.push_back("13,232,12");
+    str_lst.push_back("14,234,2332");
+
+    strtk::write_to_text_file("out.txt", str_lst, "\n");
+
+    std::deque<std::string> new_lst;
+
+    strtk::load_from_text_file("out.txt",new_lst);
+
+    std::cout << strtk::join("\n", new_lst) << std::endl;
+
     return 0;
 }
